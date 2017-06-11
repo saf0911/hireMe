114382f3c9408639c477503cda6f3df1e6ba3987
saf0911 <saf0911@gmail.com>
2017-06-10 22:48:42 -0500

    gong to bed for the night will be back in a few hours

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,11 +1,28 @@
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  avatar: String,
-  interests: String,
-  username: String
+  firstName: {
+    type: String,
+    maxlength: [ 20, 'We do not need that long of a name' ]
+  },
+  lastName: {
+    type: String,
+    maxlength: [ 20, 'That is a long last name' ]
+  },
+  avatar: {
+    type: String,
+    maxlength: [ 20, 'Pictures only' ]
+  },
+  interests: {
+    type: String,
+    maxlength: [ 300, 'Leave some information to start the conversation' ]
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    maxlength: [ 20, 'Username does not need to be that long' ]
+  }
 
 });
 
