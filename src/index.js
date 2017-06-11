2d5f80a3d07acb618149851df5d02391233d3f44
saf0911 <saf0911@gmail.com>
2017-06-11 13:15:59 -0500

    added global promise

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import AuthenicationRoute from './routes/AuthenicationRoute';
 import userRoutes from './routes/userRoutes';
+import bodyParser from 'body-parser';
 
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({
@@ -17,7 +18,10 @@ const PORT = process.env.Port || 3001;
 nextApp.prepare().then(() => {
   const app = express();
 
+  app.use(bodyParser.json());
+
   // Define all you backend handlers here...
+  mongoose.Promise = global.Promise;
   mongoose.connect('mongodb://localhost/DatingSite');
 
   const db = mongoose.connection;
